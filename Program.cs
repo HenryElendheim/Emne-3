955107779e6ca6850c7afc3bd56234c9e1c48424
HenryElendheim <henskielendheim@gmail.com>
2026-05-11 12:44:46 +0200

    Fix

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
-﻿//Console.WriteLine("Hello, World!");
+﻿using Undervisning_Emne_3;
 
-//var input = Console.ReadLine();
-//Console.WriteLine($"Du skrev: {input}");
-
-using Undervisning_Emne_3;
-
-//Datatyper + If/Else Oppgavene
-var datatyper = new Datatyper();
-//datatyper.Run();
-//datatyper.NoReturn();
-
-Console.WriteLine("(If/else - Oppgave 3) The result is:", datatyper.Conditions3(27, 3));
-
-Console.WriteLine("Test");
-
-// Stein saks papir + Random I C#
-var rockPaperScissors = new RPS();
-rockPaperScissors.Run();
+RunAll();
+void RunAll()
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("1-RPS | 2-If/Else | 3-RandomHobby");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                var rockPaperScissors = new RPS();
+                rockPaperScissors.Run();
+                break;
+            case "2":
+                var datatyper = new Datatyper();
+                datatyper.Run();
+                datatyper.NoReturn();
+                Console.WriteLine("(If/else - Oppgave 3) The result is:", datatyper.Conditions3(27, 3));
+                break;
+            case "3":
+                var RH = new RandomHobby();
+                RH.Test();
+                break;
+            default:
+                RunAll();
+                break;
+        }
+    }
+}
---
+++ RandomHobby.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Undervisning_Emne_3
+{
+    internal class RandomHobby
+    {
+        private string[] hobbies = { "Hobby 1", "Hobby 2", "Hobby 3", "Hobby 4", "Hobby 5" };
+
+        public void Test()
+        {
+            Console.WriteLine(hobbies[1]);
+        }
+    }
+}
